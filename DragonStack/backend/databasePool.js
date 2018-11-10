f757490d63e1642de0e3efba4e9d854d56e530f7
Delan Schomper <delan.schomper@gmail.com>
2018-11-11 09:00:53 +1000

    Removing unneeded files and adding database tools

---
+++ DragonStack/backend/databasePool.js
@@ -0,0 +1,15 @@
+const {Pool} = require('pg');
+const databaseConfiguration = require('./secrets/databaseConfiguration.js');
+
+const pool = new Pool(databaseConfiguration);
+
+module.exports = pool;
+
+pool.query('SELECT * FROM generation', (error, response) => {
+    if (error) {
+        console.log('error', error);
+        return;
+    }
+
+    console.log('response.rows', response.rows);
+});
