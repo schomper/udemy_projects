062629268f8b3761adab31df322b7324df573e3b
Delan Schomper <delan.schomper@gmail.com>
2018-12-02 19:03:45 +1000

    Add traits to dragons in the database

--- DragonStack/backend/app/dragon/table.js
+++ DragonStack/backend/app/dragon/table.js
@@ -1,4 +1,5 @@
 const pool = require('../../databasePool.js');
+const DragonTraitTable = require('../dragonTrait/table');
 
 class DragonTable {
     static storeDragon(dragon) {
@@ -13,7 +14,14 @@ class DragonTable {
                     if (error) return reject(error);
     
                     const dragonId = response.rows[0].id;
-                    resolve ({dragonId});
+
+                    Promise.all(dragon.traits.map(({traitType, traitValue}) => {
+                        return DragonTraitTable.storeDragonTrait({
+                            dragonId, traitType, traitValue
+                        });
+                    }))
+                    .then(() => resolve({dragonId}))
+                    .catch(error => reject(error));
                 }
             );
         });
