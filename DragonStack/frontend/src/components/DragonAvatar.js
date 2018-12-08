0bf78a303fe4f58627373bb4e2fbf19c5b983e95
Delan Schomper <delan.schomper@gmail.com>
2018-12-09 09:26:54 +1000

    Generating dragon images

--- DragonStack/frontend/src/components/DragonAvatar.js
+++ DragonStack/frontend/src/components/DragonAvatar.js
@@ -1,6 +1,56 @@
 import React, {Component} from 'react';
+import {skinny, slender, sporty, stocky, patchy, plain, spotted, stirped} from '../assets';
+
+const propertyMap = {
+    backgroundColor: {
+        black: '#263238', 
+        white: '#CFD8DC', 
+        green: '#a5d6a7', 
+        blue:  '#0277bd'
+    },
+    build: {
+        slender,
+        stocky, 
+        sporty, 
+        skinny
+    },
+    pattern: {
+        plain,
+        stirped, 
+        spotted, 
+        patchy
+    },
+    size: {
+        small:    100, 
+        medium:   140, 
+        large:    180, 
+        enromous: 220
+    }
+};
 
 class DragonAvatar extends Component {
+    get DragonImage() {
+        const dragonPropertyMap = {};
+
+        this.props.dragon.traits.forEach(trait => {
+            const {traitType, traitValue } = trait;
+
+            dragonPropertyMap[traitType] = propertyMap[traitType][traitValue];
+        });
+
+        const {backgroundColor, build, pattern, size } = dragonPropertyMap;
+
+        const sizing = {width: size, height: size}
+
+        return(
+            <div className='dragon-avatar-image-wrapper'>
+                <div className='dragon-avatar-image-background' style={{backgroundColor, ...sizing}}></div>
+                <img src={pattern} className='dragon-avatar-image-pattern' style={{...sizing}}/>
+                <img src={build} className='dragon-avatar-image' style={{...sizing}}/>
+            </div>
+        );
+    }
+
     render() {
         const { generationId, dragonId, traits }  = this.props.dragon;
 
@@ -10,6 +60,7 @@ class DragonAvatar extends Component {
                 <span>I{dragonId}. </span>
 
                 { traits.map(trait => trait.traitValue).join(', ') }
+                { this.DragonImage }
             </div>
         )
     }
