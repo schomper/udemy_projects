aab9442999b8314207fa2e29a686c38d825a3644
Delan Schomper <delan.schomper@gmail.com>
2019-01-10 10:46:52 +1000

    Create redux flow for generations

--- DragonStack/frontend/src/index.js
+++ DragonStack/frontend/src/index.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import {createStore} from 'redux';
 import {render} from 'react-dom';
 import Generation from './components/Generation';
 import Dragon from './components/Dragon';
 
 import './index.css' 
+
+const DEFAULT_GENERATION = {generationId: '', expiration: ''}
+
+const GENERATION_ACTION_TYPE = 'GENERATION_ACTION_TYPE';
+
+const generationReducer = (state, action) => {
+    if (action.type === GENERATION_ACTION_TYPE) {
+        return {generation: action.generation};
+    }
+
+    return {
+        generation: {DEFAULT_GENERATION}
+    }
+}
+const store = createStore(generationReducer);
+
+store.subscribe(() => console.log('store state update', store.getState()));
+
+store.dispatch({ type: 'foo' });
+store.dispatch({ 
+    type: GENERATION_ACTION_TYPE,
+    generation: {generationId: 'goo', expiration:'bar'}
+})
+
+console.log('store.getState()', store.getState());
+
+const generationActionCreator = (payload) => {
+    return {
+        type: GENERATION_ACTION_TYPE,
+        generation: payload
+    };
+}
+
+const zooAction = generationActionCreator({
+    generationId: 'zoo', expiration: 'bar'
+});
+
+store.dispatch(zooAction);
+
+fetch('http://localhost:3000/generation')
+    .then(response => response.json())
+    .then(json => {
+        store.dispatch(generationActionCreator(json.generation))
+    });
 
 render (
     <div>
